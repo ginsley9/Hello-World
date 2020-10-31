52f057f2900180d9f4d52616ce0c193968ed3494
Grant Insley <70414187+ginsley9@users.noreply.github.com>
2020-10-30 21:04:21 -0400

    Add files via upload

---
+++ HelloWorld1.java
@@ -0,0 +1,10 @@
+package helloworld1;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world, this is Grant Insley");
+	}
+
+}
